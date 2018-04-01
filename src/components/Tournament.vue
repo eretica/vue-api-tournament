c3b23c17dc55598832e9a7d745cb6d850ed97a31
eretica <eretica.m@gmail.com>
2018-04-01 22:59:48 +0900

    Add Request sample And include axios lib

---
+++ src/components/Tournament.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <b>
+            <router-link to="/">Go to Home</router-link>
+        </b>
+
+        <div>
+            <button @click="add()">ADD</button>
+        </div>
+
+        <div class="api-list">
+            <ul>
+                <div class="api-list__item" v-for="(api, api_index) in apis">
+                    <div>
+                        <button @click="remove(api_index)">X</button>
+                        <input v-model="api.url">
+                        <div class="api-list__item" v-for="(param, param_index) in api.params">
+                            KEY:<input type="text" v-model="param.key">
+                            VALUE:<input type="text" v-model="param.value">
+                            <button @click="removeParams(api_index, param_index)">✖</button>
+                        </div>
+                        <button @click="addParams(api_index)">➕</button>
+                        <button @click="doRequest(api_index)">📩</button>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import Axios from 'axios';
+
+  var dataTemplate = {
+    url : '',
+      params : [
+      {key: '', value: ''},
+    ],
+    done: false
+  };
+
+  export default {
+    name: 'tournament',
+    data: function () {
+      return {
+//        apis: [
+//          {
+//            url : '',
+//            params : [
+//              {key: '', value: ''},
+//            ],
+//            done: false
+//          }
+//        ]
+        apis: [
+          {
+            url : 'https://jsonplaceholder.typicode.com/posts/1',
+            params : [
+              {key: '', value: ''},
+            ],
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      add: function(event) {
+        this.apis.push(dataTemplate);
+      },
+      remove: function (key) {
+        Vue.delete(this.apis, key);
+      },
+      addParams: function (key) {
+        this.apis[key].params.push({key: '', value: ''});
+      },
+      removeParams: function (key, paramsIndex) {
+        Vue.delete(this.apis[key].params, paramsIndex);
+      },
+      doRequest: function (api_index) {
+        var api = this.apis[api_index];
+        Axios.get(api.url, api.params)
+          .then(function (result) {
+            console.log(result);
+          })
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Hello from '@/components/Hello';
+import Hello2 from '@/components/Hello2';
+import Tournament from '@/components/Tournament';
 
 Vue.use(Router);
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Hello',
+      component: Hello,
+    },
+    {
+      path: '/hello2',
+      name: 'Hello2',
+      component: Hello2,
+    },
+    {
+      path: '/tournament',
+      name: 'Tournament',
+      component: Tournament,
     },
   ],
 });
